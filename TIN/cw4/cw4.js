650a6c5179542a81aac7100a753451e0e58579ae
Jakub Cezak <jcezak5@gmail.com>
2023-10-26 13:13:48 +0200

    Update cw4.js

--- TIN/cw4/cw4.js
+++ TIN/cw4/cw4.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+// Cwiczenie A
+{
+	console.log('Hello World')
+}
+
+// Cwiczenie B
+// Zaimplementuj poniższe funkcje – jedna funkcja/obiekt + przykład wykorzystania na plik, pliki nazwij 2a.js 2b.js itd. Użyj console.log do wypisywania:
+// a)	Prezentacja działania pętli for i while (przykład dowolny)
+{
+for (i = 1; i <= 5; i++) {
+		console.log(i)
+	}
+}
+// b)	Prezentacja działania if
+
+{
+	const age = 22
+	if (age >= 21) {
+		console.log('You can legally drink in the united states of america.')
+	}
+}
+// c)	Stworzenie tablicy z kilkoma elementami
+{
+	const myArray = [1, 2, 3, 4]
+	console.log(myArray)
+}
+// d)	Funkcja przyjmująca tablicę jako parametr i wypisująca jej zawartość w kolejnych linijkach
+{
+	function printArray(array) {
+		array.forEach(element => {
+			console.log(element)
+		})
+	}
+	const myArray = [1, 2, 3, 4]
+	printArray(myArray)
+}
